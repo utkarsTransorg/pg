92940f4a305050f7241c98758f41a06749bbf0af
shubham <shubhamp@ideyalabs.com>
2025-03-31 19:04:22 +0530

    added-response

--- src/sdlccopilot/responses.py
+++ src/sdlccopilot/responses.py
@@ -30,6 +30,32 @@ class CodeResponse(BaseModel):
     messages : List[AnyMessage]
 
 
+class SecurityReview(BaseModel):
+    sec_id : str
+    review : str
+    file_path : str
+    recommendation : str
+    priority : Literal["high", "medium", "low"]
+    
+class SecurityReviewResponse(BaseModel):
+    session_id : str
+    status : Literal["in_progress", "pending_approval", "feedback", "completed"]
+    reviews : List[SecurityReview]
+    messages : List[AnyMessage]
+
+
+class TestCase(BaseModel):
+    test_id : str 
+    description : str 
+    steps : List[str]
+    status : Literal["draft", "pass", "fail"] = "draft"
+    
+    
+class TestCasesResponse(BaseModel):
+    session_id : str
+    status : Literal["in_progress", "pending_approval", "feedback", "completed"]
+    messages : List[AnyMessage]
+    test_cases : List[TestCase]
 
 #     ********* document_state :  {'functional_documents': [DocumentSection(title='INTRODUCTION', content='This document defines the functional requirements for the Password Reset 
 # Feature of the User Management System.'), DocumentSection(title='BUSINESS CONTEXT', content='The business needs a secure mechanism for users to recover access to their accounts without compromising security, improving customer satisfaction and retention.')], 'technical_documents': [], 'messages': [AIMessage(content="Please review above functional design document and provide feedback or type 'Approved' if you're satisfied.", additional_kwargs={}, response_metadata={}, id='41a4334f-abcd-4e48-a1ce-f96f36454b07')], 'document_type': 'functional', 'status': 'pending_approval', 'revised_count': 0, 'version': 1.0}
